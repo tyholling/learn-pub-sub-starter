afb600996bd0dab9b985254fac51b1092cb0d426
Tyler Hollingsworth <tyholling@gmail.com>
2025-11-17 23:09:10 +0000

    add rabbitmq connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetLevel(log.DebugLevel)
+	log.SetOutput(os.Stdout)
+	log.SetReportCaller(false)
+}
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	log.Info("server started")
+	defer log.Info("server stopped")
+
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("failed to connect to rabbitmq: %v", err)
+	}
+	defer func() {
+		if err := connection.Close(); err != nil {
+			log.Errorf("failed to close rabbitmq connection: %v", err)
+		} else {
+			log.Info("rabbitmq: connection closed")
+		}
+	}()
+	log.Info("rabbitmq: connected")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	defer fmt.Println()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/bootdotdev/learn-pub-sub-starter
 
 go 1.22.1
+
+require (
+	github.com/rabbitmq/amqp091-go v1.10.0
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
